30785396706b721ca30306ce26bbc02008a6ec0c
Derricka S <derrickas728@gmail.com>
2022-03-24 21:40:43 -0400

    add numOfChar function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,24 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var numOfChar = function(){
+  var num = prompt("How long would you like your password to be? Select a number between 8 and 128");
+  
+  num = parseInt(num);
+
+  isNum = isNaN(num); 
+
+  if (num < 7 || num > 129 || isNum ){
+    alert("Type a valid number");
+    return numOfChar();
+  } else {
+    return num;
+  }
+}
+
+
+var num = numOfChar();
+console.log(num);
 
 // Write password to the #password input
 function writePassword() {
