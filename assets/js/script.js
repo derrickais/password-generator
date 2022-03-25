36dd3348d77dfdbb52abadbe661f67c0d159899f
Derricka S <derrickas728@gmail.com>
2022-03-24 22:16:12 -0400

    added typeOfChar function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,32 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+var uppercase = [
+  "A", "B", "C", "D", "E", "F", "G", "H",
+  "I", "J", "K", "L", "M", "N", "O", "P", 
+  "Q", "R", "S", "T", "U", "V", "W", "X", 
+  "Y", "Z"
+]
+
+var lowercase = [
+  "a", "b", "c", "d", "e", "f", "g", "h", 
+  "i", "j", "k", "l", "m", "n", "o", "p", 
+  "q", "r", "s", "t", "u", "v", "w", "x", 
+  "y", "z"
+]
+
+var special = [
+  "/", "[", "!", "@", "#", "$", "%", "^",
+  "&", "*", "(", ")", "_", "+", , "-","=",
+  "[", "]", "{", "}", ";", ":", "|", ",", 
+  ".", "<", ">", "?", "]", "+","~"
+]
+
+var numeric = [
+  "0", "1", "2", "3", "4", "5", "6", "7",
+  "8", "9"
+ ]
+
 var numOfChar = function(){
   var num = prompt("How long would you like your password to be? Select a number between 8 and 128");
   
@@ -16,10 +42,28 @@ var numOfChar = function(){
   }
 }
 
-
 var num = numOfChar();
 console.log(num);
 
+var typeOfChar = function() {
+  uppercaseChar = confirm("Do you want uppercase letters in your password?");
+  lowercaseChar = confirm("Do you want lowercase letters in your password?");
+  specialChar = confirm("Do you want special characters in your password?");
+  numericChar = confirm("Do you want numbers in your password?");
+
+
+  var charType = {
+    uppercaseCon: uppercaseChar,
+    lowercaseCon: lowercaseChar,
+    specialCon: specialChar,
+    numericCon: numericChar
+  }
+  return charType;
+
+}
+
+var charType = typeOfChar();
+console.log(charType);
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
